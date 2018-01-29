274e37012b92cbe3dd72f9e6ed70e952769c4e03
rashmi33 <rashmi@gkmit.co>
2018-01-29 18:20:26 +0530

    add more javascript effects on home.html

--- assets/javascripts/home.js
+++ assets/javascripts/home.js
@@ -9,58 +9,79 @@ JS.Home.prototype = {
 		this.changeTextColorOnHover();
 		this.openModalPopup();
 		this.getDataFromModalPopupForm();
+		this.addHoveronTeamImages();
+		this.addHoveronFooterText();
 	},
 
 	changeTextColorOnHover: function(){
-		document.getElementById("work-p").onmouseover = function() { mouseOver() };
-		document.getElementById("work-p").onmouseout = function() { mouseOut() };
+		document.getElementById('work-p').onmouseover = function() { mouseOver() };
+		document.getElementById('work-p').onmouseout = function() { mouseOut() };
 		function mouseOver() {
-      document.getElementById("work-p").style.color = "red";
+      document.getElementById('work-p').style.color = 'red';
 		}
 		function mouseOut() {
-      document.getElementById("work-p").style.color = "white";
+      document.getElementById('work-p').style.color = 'white';
 		}
 	},
 
   openModalPopup: function(){
     var form2 = document.getElementById('myModal');
-		var btn2 = document.getElementById("signup2");
-		var btn3 = document.getElementById("signup3");
-		var span = document.getElementsByClassName("close")[0];
+		var btn2 = document.getElementById('signup2');
+		var btn3 = document.getElementById('signup3');
+		var span = document.getElementsByClassName('close')[0];
 	  btn2.onclick = function() {
-	    form2.style.display = "block";
+	    form2.style.display = 'block';
 	  }
 
 	  btn3.onclick = function() {
-	    form2.style.display = "block";
+	    form2.style.display = 'block';
 	  }
 
 		span.onclick = function() {
-	    form2.style.display = "none";
+	    form2.style.display = 'none';
 	  }
 
 	  window.onclick = function(event) {
 	    if (event.target == form2) {
-	        form2.style.display = "none";
+	        form2.style.display = 'none';
 	    } 
 	  }
   },
 
   getDataFromModalPopupForm: function(){
-  	$('#modalForm').submit(function() {
+  	$('#myFormModal #modalForm').submit(function() {
   	var username = document.getElementById('username').value;
   	var display = document.getElementById('username1');
     display.value= username;
     return false;
     });
 	  var form1 = document.getElementById('myFormModal');
-	  var btn1 = document.getElementById("signup1");
-	  var span1 = document.getElementsByClassName("formclose")[0];
+	  var btn1 = document.getElementById('signup1');
+	  var span1 = document.getElementsByClassName('formclose')[0];
 	  btn1.onclick = function() {
-	    form1.style.display = "block";
+	    form1.style.display = 'block';
 	  }
 	  span1.onclick = function() {
-	    form1.style.display = "none";
+	    form1.style.display = 'none';
 	  }
-  }
+  },
+
+  addHoveronTeamImages: function(){
+  	$('#team img').hover(
+      function(){
+        $(this).next().css('visibility','visible');
+       }, function(){
+        $(this).next().css('visibility','hidden');
+       }
+    );
+  },
+
+  addHoveronFooterText: function(){
+	  $('#footer p').mouseover(function(){
+	    $('#footer p').css('background-color', 'yellow');
+	  });
+	  $('p').mouseout(function(){
+	    $('#footer p').css('background-color', 'black');
+	  });
+	}
 }
