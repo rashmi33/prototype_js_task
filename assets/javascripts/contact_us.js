991776392cc58e254f6f2df11303265b18565a97
rashmi33 <rashmi@gkmit.co>
2018-01-30 11:36:17 +0530

    Add validation on contact_us modal popup display

--- assets/javascripts/contact_us.js
+++ assets/javascripts/contact_us.js
@@ -20,21 +20,25 @@ JS.Contact.prototype = {
 	},
 
 	openModalPopupOnContact: function(){
-    var form = document.getElementById('contact-modal');
-		var btn = document.getElementById('contact-button');
-		var span = document.getElementsByClassName('close')[0];
-	  btn.onclick = function() {
-	    form.style.display = 'block';
-	  }
+		$('#contact-us-form').submit(function() {
+			var user = document.getElementById('username').value;
+		  var emai = document.getElementById('email').value;
+		  var msg = document.getElementById('message').value;
+			if(user != null && emai != null && msg != null){
+		    var form = document.getElementById('contact-modal');
+				var btn = document.getElementById('contact-button');
+				var span = document.getElementsByClassName('close')[0];
+		    form.style.display = 'block';
+			  span.onclick = function() {
+		      form.style.display = 'none';
+		    }
 
-		span.onclick = function() {
-	    form.style.display = 'none';
-	  }
-
-	  window.onclick = function(event) {
-	    if (event.target == form) {
-	        form.style.display = 'none';
-	    } 
-	  }
+			  window.onclick = function(event) {
+			    if (event.target == form) {
+			      form.style.display = 'none';
+			    } 
+			  }
+			}
+	  });
   }
 }
